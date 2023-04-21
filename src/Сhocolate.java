2b56062d62b86c72cf21f8dbe06e63bd5e94a854
Valeriy <apachege@bk.ru>
2023-04-21 18:21:36 +0400

    Commit2

--- src/Сhocolate.java
+++ src/Сhocolate.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Сhocolate {
+public class Сhocolate extends Product {
+    private final String type;
+
+    public Сhocolate(String name, Integer cost, String type) {
+        super(name, cost);
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + type;
+    }
 }
